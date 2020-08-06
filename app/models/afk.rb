21dd57c2d883b1a5fd75f4186f3438080cf77983
pyama86 <pyama@pepabo.com>
2020-08-06 15:10:07 +0900

    add tips

--- app/models/afk.rb
+++ app/models/afk.rb
@@ -21,10 +21,15 @@ module App
           bot_token_client.chat_postMessage(channel: params["channel_id"], text: "#{params["user_name"]}が離席しました。代わりに不在をお伝えします",  as_user: true)
         end
 
+        tips = [
+          "\nちなみに通知も止めたいときは `/and 15min` とか `/dnd 1h` とかで通知も止められるし、 `/dnd off` で元に戻せるよ、知ってるとは思ったんだけど、念の為ね、知ってるとは思ったんだけどね",
+          "\nちなみに、リマインドって知ってる？ `/remind @pyama この内容を15分後に@pyamaに通知するよ in 15min` とか `/remind @pyama この内容を1時間後に通知するよ in 1h` とかすると、15分後とか1時間後に @pyama さんに通知できるんだ、相手を気遣って時間ずらしてメンションしたいときとか使ってみてよ、いや、知ってるとは思ったんだけどね。`/remind @pyama この内容を明日の10時に通知するよ in tomorrow at 10am` とかは流石に知らなかったでしょ？僕もさっき知ったしね、まあ知ってるとは思ったんだけどね。自分に設定したいときは `/remind` だけ押してエンター押してみてよ。"
+        ]
+
         unless params["minute"].empty?
-          "行ってらっしゃい!!1 #{(Time.now + diff).strftime("%H:%M")}に自動で解除します"
+          "行ってらっしゃい!!1 #{(Time.now + diff).strftime("%H:%M")}に自動で解除します" + tips.sample
         else
-          "行ってらっしゃい!!1"
+          "行ってらっしゃい!!1" + tips.sample
         end
       end
     end
